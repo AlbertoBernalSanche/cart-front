637451542cf8004e2a4f8134371f8aed61ac40d8
Alberto Bernal Sanchez <abernals@correo.usbcali.edu.co>
2020-11-09 09:58:16 -0500

    cambios para seguridad

--- src/app/service/payment-method.service.ts
+++ src/app/service/payment-method.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -13,27 +13,39 @@ export class PaymentMethodService {
 
   constructor(public httpClient:HttpClient) { }
 
+  createTokenHeader():HttpHeaders{
+    let token=localStorage.getItem('token');
+    let headers=new HttpHeaders({'Authorization':token});
+    return headers;
+  }
+
+
   public findAll():Observable<any>{
-    return this.httpClient.get(this.url+'findAll');
+    let headers=this.createTokenHeader();
+    return this.httpClient.get(this.url+'findAll',{headers:headers});
   }
 
   public findById(payId:string):Observable<any>{
-    return this.httpClient.get(this.url+'findById/'+payId);
+    let headers=this.createTokenHeader();
+    return this.httpClient.get(this.url+'findById/'+payId,{headers:headers});
   }
 
   public delete(payId:string):Observable<any>{
-    return this.httpClient.delete(this.url+'delete/'+payId);
+    let headers=this.createTokenHeader();
+    return this.httpClient.delete(this.url+'delete/'+payId,{headers:headers});
   }
 
   public save(paymentMethod:PaymentMethod):Observable<any>{
+    let headers=this.createTokenHeader();
 
-    return this.httpClient.post(this.url+'save',paymentMethod);
+    return this.httpClient.post(this.url+'save',paymentMethod,{headers:headers});
 
   }  
 
   public update(paymentMethod:PaymentMethod):Observable<any>{
+    let headers=this.createTokenHeader();
 
-    return this.httpClient.put(this.url+'update',paymentMethod);
+    return this.httpClient.put(this.url+'update',paymentMethod,{headers:headers});
 
   }
 }
