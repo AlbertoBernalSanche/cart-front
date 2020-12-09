ec228e32e3b8785a778949f078080cc3c9ef1dc4
Alberto Bernal Sanchez <abernals@correo.usbcali.edu.co>
2020-12-09 08:04:04 -0500

    eliminar comentarios inutiles

--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -68,10 +68,6 @@ export class RegisterComponent implements OnInit {
 
       }).catch(e => {
 
-        /*this.customerService.delete(this.email).subscribe(ok=>{
-
-          console.log("eliminacion de registro por fallo de firebase")
-        })*/
         console.log(e);
         this.showMsg = true;
         this.messages = e.error.error;
